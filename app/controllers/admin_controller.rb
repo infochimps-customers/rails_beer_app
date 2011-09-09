3163861e30fdb47c49849feb46c0e3bc326e003b
Dhruv Bansal <dhruv@ph.utexas.edu>
2011-09-09 15:34:54 -0500

    Twutils no longer needed -- and have to use numeric links when strong_links comes back

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -25,23 +25,15 @@ class AdminController < ApplicationController
               }
              ]
     @customers.each do |customer|
-      trstrank = infochimps_api_request("/social/network/tw/influence/trstrank", :screen_name => customer[:twitter])
+      
+      trstrank            = infochimps_api_request("/social/network/tw/influence/trstrank", :screen_name => customer[:twitter])
       customer[:trstrank] = trstrank['trstrank']
       
-      wordbag = infochimps_api_request("/social/network/tw/token/wordbag", :screen_name => customer[:twitter])
+      wordbag            = infochimps_api_request("/social/network/tw/token/wordbag", :screen_name => customer[:twitter])
       customer[:wordbag] = (wordbag['tokens'] || []).reject { |token, weight| token =~ /beer/ }.sort { |token, weight| weight[1] }.reverse[0..30].map(&:first).sort
 
-      strong_links    = infochimps_api_request("/social/network/tw/graph/strong_links", :screen_name => customer[:twitter])
-      strong_link_ids = (strong_links["strong_links"] ||[]).sort { |user_id, weight| weight[1] }.reverse.map(&:first)[0..5] # CAREFUL ADJUSTING THIS!
-
-      strong_link_screen_names = [].tap do |us|
-        strong_link_ids.each do |user_id|
-          whois = infochimps_api_request("/social/network/tw/util/map_id", :user_id => user_id)
-          us << whois["screen_name"]
-        end
-      end
-      customer[:strong_links] = strong_link_screen_names
-      
+      strong_links            = infochimps_api_request("/social/network/tw/graph/strong_links", :screen_name => customer[:twitter])
+      customer[:strong_links] = (strong_links["strong_links"] ||[]).sort { |user_id, weight| weight[1] }.reverse.map(&:first)[0..5]
     end
     
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
-  def link_to_twitter_profile screen_name
-    link_to("@" + screen_name, "http://twitter.com/" + screen_name, :target => :_blank)
+  def link_to_twitter_profile screen_name_or_user_id
+    return unless screen_name_or_user_id.present?
+    if screen_name_or_user_id.is_a?(Fixnum)
+      base_url = "http://twitter.com/account/redirect_by_id?id="
+      text     = screen_name_or_user_id.to_s
+    else
+      base_url = "http://twitter.com/"
+      text     = "@" + screen_name_or_user_id.to_s
+    end
+    link_to(text, base_url + screen_name_or_user_id.to_s, :target => :_blank)
   end
 
   def highlight_demo_params text
